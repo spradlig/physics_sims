8c041ce03bf5654914c87d84eeab72b82a1ca12e
Gabe Spradlin <gabe.spradlin@gmail.com>
2022-12-12 13:30:00 -0700

    simple event driven model commit

---
+++ tower_defense/configs/__init__.py
@@ -0,0 +1,29 @@
+"""
+Module:
+    __init__.py
+
+Description:
+    This file allows for the code in this package to operate similar to any pip/conda installed
+    package.
+
+Usage:
+    N/A
+
+License:
+    https://creativecommons.org/licenses/by-nc-nd/4.0/
+    Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+    See LICENSE.txt
+
+"""
+
+"""
+Version History:
+    Original:
+        Gabe Spradlin | 12-Dec-2022
+"""
+
+"""
+TODOs:
+    1)
+"""
+
---
+++ tower_defense/env/coordinates.py
@@ -0,0 +1,39 @@
+"""
+Module:
+    coordinates.py
+
+Description:
+    <Short description, but thorough, of what is included in the file.>
+
+Usage:
+    <from some_module import some_function>
+    <Provide a simple example for each class and function in the file.>
+
+Notes:
+
+
+References:
+
+
+License:
+    https://creativecommons.org/licenses/by-nc-nd/4.0/
+    Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+    See LICENSE.txt
+
+"""
+
+"""
+Version History:
+    Original:
+        Gabe Spradlin | 12-Dec-2022
+"""
+
+"""
+TODOs:
+    1)
+"""
+
+# Standard library imports
+
+
+# Tool imports
